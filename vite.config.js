3dd21aa07512a73a34da2de554350403ed62a10f
HaimFC <haimtehem@gmail.com>
2025-08-08 23:40:46 +0300

    update files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-vite-site/"
-});
+  base: '/QuickNotes-React-Project/'
+})
